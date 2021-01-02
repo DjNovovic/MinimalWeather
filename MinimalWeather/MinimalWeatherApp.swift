da786602edfd75798e4397b00595cff39b682d4f
DjNovovic <djordjijenovovic@gmail.com>
2021-01-02 16:53:17 +0100

    Initial Commit

---
+++ MinimalWeather/MinimalWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalWeatherApp.swift
+//  MinimalWeather
+//
+//  Created by Đorđije Novović on 2.1.21..
+//
+
+import SwiftUI
+
+@main
+struct MinimalWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
